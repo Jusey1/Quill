1e8c35e1eff9a2cdf56102cb34831b7c30c1dc40
Salju <61861078+Jusey1@users.noreply.github.com>
2024-12-11 21:35:26 -0500

    Add files via upload

--- src/main/java/net/salju/quill/mixins/ItemRendererMixin.java
+++ src/main/java/net/salju/quill/mixins/ItemRendererMixin.java
@@ -1,13 +1,11 @@
 package net.salju.quill.mixins;
 
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import net.salju.quill.Quill;
 import net.salju.quill.item.BundleHoldingItem;
 import net.minecraft.client.resources.model.BakedModel;
@@ -20,6 +18,7 @@ import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import com.mojang.blaze3d.vertex.PoseStack;
+import org.jetbrains.annotations.Nullable;
 
 @Mixin(ItemRenderer.class)
 public abstract class ItemRendererMixin {
@@ -30,10 +29,10 @@ public abstract class ItemRendererMixin {
 	@Inject(method = "render", at = @At(value = "HEAD"), cancellable = true)
 	private void renderBundle(ItemStack stack, ItemDisplayContext context, boolean check, PoseStack pose, MultiBufferSource buffer, int c, int e, BakedModel model, CallbackInfo ci) {
 		if (stack.getItem() instanceof BundleHoldingItem target) {
-			if (target.getSelectedItem(stack) != -1) {
-				this.renderItemModelRaw(stack, context, check, pose, buffer, c, e, this.resolveModelOverride(this.itemModelShaper.getItemModel(ResourceLocation.fromNamespaceAndPath(Quill.MODID, "bundle_open_back")), stack, null, null, 0), this.quill$shouldRenderItemFlat(context), -1.5F);
-				this.renderSimpleItemModel(target.getContents(stack).get(target.getSelectedItem(stack)), context, check, pose, buffer, c, e, this.getModel(target.getContents(stack).get(target.getSelectedItem(stack)), null, null, 0), this.quill$shouldRenderItemFlat(context));
-				this.renderItemModelRaw(stack, context, check, pose, buffer, c, e, this.resolveModelOverride(this.itemModelShaper.getItemModel(ResourceLocation.fromNamespaceAndPath(Quill.MODID, "bundle_open_front")), stack, null, null, 0), this.quill$shouldRenderItemFlat(context), 0.5F);
+			if (target.getSelectedItem(stack) != -1 && context == ItemDisplayContext.GUI) {
+				this.renderItemModelRaw(stack, context, check, pose, buffer, c, e, this.resolveModelOverride(this.itemModelShaper.getItemModel(ResourceLocation.fromNamespaceAndPath(Quill.MODID, "bundle_open_back")), stack, null, null, 0), true, -1.5F);
+				this.renderSimpleItemModel(target.getContents(stack).get(target.getSelectedItem(stack)), context, check, pose, buffer, c, e, this.getModel(target.getContents(stack).get(target.getSelectedItem(stack)), null, null, 0), true);
+				this.renderItemModelRaw(stack, context, check, pose, buffer, c, e, this.resolveModelOverride(this.itemModelShaper.getItemModel(ResourceLocation.fromNamespaceAndPath(Quill.MODID, "bundle_open_front")), stack, null, null, 0), true, 0.5F);
 				ci.cancel();
 			}
 		}
@@ -50,9 +49,4 @@ public abstract class ItemRendererMixin {
 
 	@Shadow
 	public abstract BakedModel getModel(ItemStack stack, @Nullable Level world, @Nullable LivingEntity owner, int i);
-
-	@Unique
-	private boolean quill$shouldRenderItemFlat(ItemDisplayContext context) {
-		return context.equals(ItemDisplayContext.GUI) || context.equals(ItemDisplayContext.GROUND) || context.equals(ItemDisplayContext.FIXED);
-	}
 }
