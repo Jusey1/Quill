a45f78e449b406c8c1e96c14bb9bec9f4a8ac55f
Salju <61861078+Jusey1@users.noreply.github.com>
2025-04-11 15:25:38 -0400

    Reset

--- src/main/java/net/salju/quill/item/component/BundleHoldingTooltip.java
+++
@@ -1,9 +0,0 @@
-package net.salju.quill.item.component;
-
-import net.minecraft.world.inventory.tooltip.TooltipComponent;
-
-public record BundleHoldingTooltip(BundleHoldingContents contents) implements TooltipComponent {
-    public BundleHoldingContents getContents() {
-        return this.contents;
-    }
-}
